b362a091612765433b33d7e25975e9d9d1c1f2f5
jazzqi <jazzqi@gmail.com>
2012-10-27 23:36:49 +0800

    No need to close the db connection after crud or excpetions
    使用ConnectionFactory管理了一个线程同步的conn, 因此每次操作完成后不需要关闭conn

--- CRM/src/real/action/dao/LoginDao.java
+++ CRM/src/real/action/dao/LoginDao.java
@@ -1,26 +1,29 @@
 package real.action.dao;
-import real.action.sql.*;
-import java.sql.*;
-public class LoginDao{
-	public static boolean isLogin(String uname,String passwd){
-		Connection conn=null;
-		PreparedStatement stm=null;
-		ResultSet rs=null;
-		String sql="select * from users where u_name=? and u_password=?";
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import real.action.sql.ConnectionFactory;
+
+public class LoginDao {
+	public static boolean isLogin(String uname, String passwd) {
+		Connection conn = null;
+		PreparedStatement stm = null;
+		ResultSet rs = null;
+		String sql = "select * from users where u_name=? and u_password=?";
 		try {
-			conn=ConnectionFactory.getConnection();
-			stm=conn.prepareStatement(sql);
+			conn = ConnectionFactory.getConnection();
+			stm = conn.prepareStatement(sql);
 			stm.setString(1, uname);
 			stm.setString(2, passwd);
-			rs=stm.executeQuery();
-			if(rs.next())	return true;
-			else	return false;
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
+			rs = stm.executeQuery();
+			if (rs.next())
+				return true;
+			else
+				return false;
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			// 因为使用ConnectionFactory工厂类维护一个线程安全的connection, 执行完成或发生异常时并不需要关闭conn!
 			e.printStackTrace();
 			return false;
 		}
